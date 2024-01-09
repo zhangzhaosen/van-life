e2fe6389f685cf09352b2ac973c117aeae95f732
zhangzhaosen <zhangzhaosen@sina.com>
2024-01-09 09:13:09 +0800

    vans list bug fix

--- index.jsx
+++ index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from './pages/Vans';
 
 function App() {
   return (
