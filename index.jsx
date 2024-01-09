f14acc78364ee08797e359f912a667d38dedda61
zhangzhaosen <zhangzhaosen@sina.com>
2024-01-09 09:09:35 +0800

    vans list

--- index.jsx
+++ index.jsx
@@ -11,11 +11,13 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   )
